78cc60cf2c2fb7a2252b19288b565597a589e030
Scott Crowder <scrowder@gmail.com>
2024-09-26 09:41:11 -0500

    Initial Commit

---
+++ ReceipeDisplay/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// File: ContentView.swift
+// Project: ReceipeDisplay
+// 
+// Created by SCOTT CROWDER on 9/26/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceipeDisplay/ReceipeDisplayApp.swift
@@ -0,0 +1,20 @@
+//
+// File: ReceipeDisplayApp.swift
+// Project: ReceipeDisplay
+// 
+// Created by SCOTT CROWDER on 9/26/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct ReceipeDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
